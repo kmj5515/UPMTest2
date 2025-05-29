4e3278249f163ebe2c2941820a4ef489cdf42ff4
김민준 <79951019+kmj5515@users.noreply.github.com>
2025-05-30 03:25:02 +0900

    ssaas

---
+++ SDK/MyTestClass.cs
@@ -0,0 +1,12 @@
+// 경로: Assets/Test/PPL/MyTestClass.cs
+
+namespace Test.PPL
+{
+    public class MyTestClass
+    {
+        public static string GetMessage()
+        {
+            return "Hello from Test.PPL!";
+        }
+    }
+}
---
+++ Sample~/Sample/Scenes/SampleUsage.cs
@@ -0,0 +1,12 @@
+// 경로: Assets/Scripts/SampleUsage.cs
+
+using UnityEngine;
+//using Test.PPL;
+
+public class SampleUsage : MonoBehaviour
+{
+    void Start()
+    {
+        //Debug.Log(MyTestClass.GetMessage());
+    }
+}
